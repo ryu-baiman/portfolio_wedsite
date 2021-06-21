8cdfc86cce03a8411145e47b7055f28301ab154d
ryu-baiman <75413211+ryu-baiman@users.noreply.github.com>
2021-06-21 15:41:57 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -15,11 +15,84 @@ $(function()  {
     });
 
     // 各項目までスクロール
-    $('header a').click(function()  {
+    $('a[href^="#"]').click(function()  {
+        var speed = 500;
         var id = $(this).attr('href');
         var position = $(id).offset().top;
         $('html, body').animate({
           'scrollTop': position
+        }, speed, 'swing');
+        return false;
+    });
+
+    //ロード時に表示
+    load_effect();
+    function load_effect(){
+        var tt = $(window).scrollTop();
+        var hh = $(window).height();
+        $('.main').each(function(){
+            var yy = $(this).offset().top;
+            if (tt > yy - hh){
+                $(this).addClass('done');
+            }
+        });
+    }
+
+    //フェードイン
+    $(window).scroll(function (){
+        $(".myself").each(function(){
+          var imgPos = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > imgPos - windowHeight + windowHeight/5){
+            $(this).addClass("fade_on");
+          } else {
+            $(this).removeClass("fade_on");
+          }
         });
       });
+    
+    //HTMLの場合
+    $(window).scroll(function (){
+      $(".html").each(function(){
+        var imgPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight - 150){
+          $(this).addClass("below");
+        } else {
+          $(this).removeClass("below");
+        }
+      });
+    });
+
+    $(window).scroll(function (){
+      $(".js").each(function(){
+        var imgPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight - 150){
+          $(this).addClass("below");
+        } else {
+          $(this).removeClass("below");
+        }
+      });
+    });
+
+    $(window).scroll(function (){
+      $(".wp").each(function(){
+        var imgPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight - 150){
+          $(this).addClass("below");
+        } else {
+          $(this).removeClass("below");
+        }
+      });
+    });
+
+    
+    
 });
+
